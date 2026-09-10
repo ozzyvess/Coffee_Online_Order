95b857dfbf58540811beb8c34ad5614dd5510e03
ozzyvess <dennygunawannn8@gmail.com>
2026-09-10 15:44:38 +1200

    Initial project setup with mySQYL configuration

---
+++ src/main/java/nz/ac/aut/comp713/coffee/CoffeeOnlineOrderApplication.java
@@ -0,0 +1,11 @@
+package nz.ac.aut.comp713.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOnlineOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeOnlineOrderApplication.class, args);
+    }
+}
